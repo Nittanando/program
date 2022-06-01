7dea7787e975a93900c871f3b898dd498a5e37d9
nittanando <sarkar.anando2995@gmail.com>
2022-06-01 17:51:11 +0600

    gallery updated

--- priyanka-master/new_front-end/asset/js/script.js
+++ priyanka-master/new_front-end/asset/js/script.js
@@ -1,18 +1,47 @@
 // filterizer
 
-$(document).ready(function () {
-  var containerEl = document.querySelector(".filtr-container");
-  var filterizd;
-  if (containerEl) {
-    filterizd = $(".filtr-container").filterizr({});
-  }
-  //Active changer
-  $(".filter-controls li").on("click", function () {
-    $(".filter-controls li").removeClass("active");
-    $(this).addClass("active");
-  });
-});
+// $(document).ready(function () {
+//   var containerEl = document.querySelector(".filtr-container");
+//   var filterizd;
+//   if (containerEl) {
+//     filterizd = $(".filtr-container").filterizr({});
+//   }
+//   //Active changer
+//   $(".filter-controls li").on("click", function () {
+//     $(".filter-controls li").removeClass("active");
+//     $(this).addClass("active");
+//   });
+// });
 
 // light gallery intializer
 
-lightGallery(document.getElementById("lightgallery"));
+// lightGallery(document.getElementById("lightgallery"));
+
+// gallery
+
+$(document).ready(function () {
+  $(".buttons").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+
+    var filter = $(this).attr("data-filter");
+
+    if (filter == "all") {
+      $(".image").show(400);
+    } else {
+      $(".image")
+        .not("." + filter)
+        .hide(200);
+      $(".image")
+        .filter("." + filter)
+        .show(400);
+    }
+  });
+
+  $(".gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+});
